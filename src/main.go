5ceb2669ebaa545cda4a6e44df887a18b0e27849
Per Arneng <per.arneng@anyplanet.com>
2010-01-11 22:50:41 +0100

    added som initial tcp listener code

--- src/main.go
+++ src/main.go
@@ -3,12 +3,35 @@ package main
 import (
 	"fmt"
 	"netsnail"
+    "net"
+    "os"
 )
 
 func main() {
 
 	conf := netsnail.NewConfig()
 	conf.ParseArgs()
+
+    tcpAddress, err := 
+        net.ResolveTCPAddr(fmt.Sprintf("localhost:%d", conf.LocalPort))
+    if err != nil {
+        fmt.Printf("failed %s\n", err)
+        os.Exit(1)
+    }
+
+    listener, err := net.ListenTCP("tcp", tcpAddress)
+    if err != nil {
+        fmt.Printf("failed %s\n", err)
+        os.Exit(1)
+    }
+
+
+    con, err := listener.AcceptTCP()
+    
+    err := con.Close()
+
+
+    listener.Close()
 
 	fmt.Printf("netsnail %s\n", conf)
 }
